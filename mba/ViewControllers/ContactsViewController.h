93091888dd97aca7a17ffbdaaf56a3ab9131866f
lad <lad@archermindtekiMac-mini.local>
2014-07-11 16:51:55 +0800

    contact

--- mba/ViewControllers/ContactsViewController.h
+++ mba/ViewControllers/ContactsViewController.h
@@ -8,9 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ContactsViewController : BaseViewController<UITableViewDataSource,UITableViewDelegate>{
+@interface ContactsViewController : BaseViewController<UITableViewDataSource,UITableViewDelegate,UIScrollViewDelegate>{
+    
+    __weak IBOutlet UISegmentedControl *_segmentControl;
+    __weak IBOutlet UITableView *_tableViewPeople;
+    __weak IBOutlet UIScrollView *_scrollView;
+    
+    __weak IBOutlet UITableView *_tableViewCollect;
     
 }
 
-- (IBAction)onTopTypeChanged:(id)sender;
 @end
--- mba/ViewControllers/ContactsViewController.m
+++ mba/ViewControllers/ContactsViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ContactsViewController.h"
-
+#import <QuartzCore/QuartzCore.h>
 @interface ContactsViewController ()
 
 @end
@@ -17,13 +17,38 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [_scrollView setContentSize:CGSizeMake(2 * (self.view.bounds.size.width), 0)];
+     [_segmentControl setBackgroundImage:[UIImage imageNamed:@"left_on_right_off"]  forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    [_segmentControl addTarget:self action:@selector(chooseTableView) forControlEvents:UIControlEventValueChanged];
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark segmentControl Method
+- (void)chooseTableView
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [_scrollView setContentOffset:CGPointMake(self.view.size.width * _segmentControl.selectedSegmentIndex, -64.0f) animated:YES];
+    if (0 == _segmentControl.selectedSegmentIndex) {
+        [_segmentControl setBackgroundImage:[UIImage imageNamed:@"left_on_right_off"]  forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    } else {
+        [_segmentControl setBackgroundImage:[UIImage imageNamed:@"left_off_right_on"]  forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    }
+}
+
+#pragma mark  UIScrollViewDelegate Method
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    int page = _scrollView.contentOffset.x / self.view.bounds.size.width;
+    _segmentControl.selectedSegmentIndex = page;
+    if (0 == page) {
+         [_segmentControl setBackgroundImage:[UIImage imageNamed:@"left_on_right_off"]  forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    } else {
+         [_segmentControl setBackgroundImage:[UIImage imageNamed:@"left_off_right_on"]  forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    }
+}
+
+#pragma mark  UITableViewDelegate Method
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 99;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -31,10 +56,14 @@
     return 10;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+         [self performSegueWithIdentifier:@"UserInfo" sender:nil];
+}
+
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"ContactCell";
-    
     UITableViewCell *cell = [tableView
                              dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
@@ -42,10 +71,21 @@
                 initWithStyle:UITableViewCellStyleDefault
                 reuseIdentifier:CellIdentifier];
     }
-    
+    UIButton *btImg = (UIButton *)[cell viewWithTag:101];
+    [btImg.layer setCornerRadius:10.0f];
+    btImg.layer.masksToBounds = YES;
+    UIButton *btAttention = (UIButton *)[cell viewWithTag:100];
+    [btAttention.layer setCornerRadius:5];
+    if (tableView == _tableViewPeople) {
+        btAttention.hidden = YES;
+    }
     return cell;
 }
 
-- (IBAction)onTopTypeChanged:(id)sender {
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
 }
+
 @end
